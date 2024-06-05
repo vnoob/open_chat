2c096e464f099c73669fc38d8db4027fd1dbcf29
vincent.nguyen <hvu.nguyen2007@gmail.com>
2024-06-05 16:04:28 +0700

    Change variable to env

--- open_chat.py
+++ open_chat.py
@@ -60,20 +60,20 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
-    genai.configure(api_key="AIzaSyBqEm-UEKmRoQdKv1u_fOnpza5mue5GdFE")
+    genai.configure(api_key=os.environ.get('GEMINI_BOT_TOKEN'))
 
     # Create the model
     # See https://ai.google.dev/api/python/google/generativeai/GenerativeModel
     generation_config = {
         "temperature": 1,
         "top_p": 0.95,
-        "top_k": 64,
-        "max_output_tokens": 8192,
+        "top_k": 128,
+        "max_output_tokens": int(os.environ.get('MAX_OUTPUT_TOKEN')),
         "response_mime_type": "text/plain",
     }
 
     model = genai.GenerativeModel(
-        model_name="gemini-1.5-pro",
+        model_name=os.environ.get('GEMINI_MODEL'),
         generation_config=generation_config,
         # safety_settings = Adjust safety settings
         # See https://ai.google.dev/gemini-api/docs/safety-settings
@@ -91,7 +91,7 @@ load_dotenv()
 def main() -> None:
     """Start the bot."""
     # Create the Application and pass it your bot's token.
-    application = Application.builder().token("7174414575:AAEapQ55TOycYd4UdRQtoa7B603u6ozxOfU").build()
+    application = Application.builder().token(os.environ.get('TELEGRAM_BOT_TOKEN')).build()
 
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
