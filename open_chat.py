003a79e083e406e7467e1c0fbe1030f002848f08
vincent.nguyen <hvu.nguyen2007@gmail.com>
2024-06-06 17:11:24 +0700

    Change output_token

--- open_chat.py
+++ open_chat.py
@@ -79,12 +79,12 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         "temperature": 1,
         "top_p": 0.95,
         "top_k": 128,
-        "max_output_tokens": 1048576,
+        "max_output_tokens": 8192,
         "response_mime_type": "text/plain",
     }
 
     model = genai.GenerativeModel(
-        model_name="gemini-1.5-pro",
+        model_name="gemini-1.5-flash",
         generation_config=generation_config,
         # safety_settings = Adjust safety settings
         # See https://ai.google.dev/gemini-api/docs/safety-settings
